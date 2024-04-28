e8fa36d114bd684d4893824010ef4a1370c36bfb
Franco904 <francostavares2003@gmail.com>
2024-04-28 17:08:14 -0300

    adiciona funcao utilitaria de arquivo

--- src/main/kotlin/utils/FileUtils.kt
+++ src/main/kotlin/utils/FileUtils.kt
@@ -2,11 +2,13 @@ package utils
 
 import java.io.File
 
-fun File.putLine(text: String) = appendText(text)
-
 fun File.getLine(text: String) = bufferedReader().readLines().find { text in it }
 
 fun File.getFirstLine() = bufferedReader().readLines().first()
+
+fun File.hasLine(text: String) = bufferedReader().readLines().any { text in it }
+
+fun File.putLine(text: String) = appendText(text)
 
 fun File.removeLine(text: String) {
     val linesToKeep = bufferedReader().readLines().filterNot { text in it }
